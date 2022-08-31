d4c9008f9ec5e432aa368da26811de2dcba39868
«Andrey» <khomehko@mail.ru>
2022-09-01 01:35:16 +0300

    h59

---
+++ h59/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+// на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+Console.Write("Введите число M : ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Fill2DArr(M, N, -10, 10);
+Console.WriteLine("Исходный массив");
+PrintArray(array); // Вызов метода печати массива
+SwapFirstAndLastRows(array);// Вызов метода печати перевернутого массива
+Console.WriteLine();
+Console.WriteLine("Перевернутый массив");
+PrintArray(array);
+
+// Метод возвращает двумерный массив [,] на size элементов 
+int[,] Fill2DArr(int rowsCount, int columnsCount, int lefts, int rights)
+{
+    int[,] array = new int[rowsCount, columnsCount];
+
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(lefts, rights);
+        }
+    }
+    {
+
+    }
+    return array;
+}
+// Метод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+// Метод  поиска  минимального элемента массива
+void SwapFirstAndLastRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int min = int.MinValue;
+
+
+    //    int temp=array[0,i];
+    //    array[0,i]=array[array.GetLength(0)-1 ,i];
+    //    array[array.GetLength(0)-1 ,i]=temp;
+    }
+
+}
